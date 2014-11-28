2ec2177e2a3cd5bf6b601a9d917565f7c4ad1755
Sequeiros <rafanetow@gmail.com>
2014-11-27 23:17:30 -0700

    Fist Comit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_dos_programacon_paralela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*for (int i = 0; i < 10; i++)
+            {
+                long total = GetTotal();
+
+                Console.WriteLine(total + " ," + i);
+            }*/
+            
+            Parallel.For(0, 10, i =>
+            {
+                long total = GetTotal();
+                Console.WriteLine(total+ " ,"+i);
+            });
+            Console.ReadLine();
+                 
+        }
+
+
+        public static long GetTotal()
+        {
+            long total = 0;
+            for (int i = 0; i < 100000000; i++)
+            {
+                total++;
+            }
+            return total;
+        }
+       
+    }
+
+}
